d4421279d5108f74ec76560d35c66b9077b713b5
kycnexynpuwejl <kycnexynpuwejl@protonmail.com>
2022-06-02 21:31:36 +0300

    add UI to IdentityServer

--- IS4TEST.IdentityServer/Startup.cs
+++ IS4TEST.IdentityServer/Startup.cs
@@ -11,8 +11,7 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        // uncomment, if you want to add an MVC-based UI
-        //services.AddControllersWithViews();
+        services.AddControllersWithViews();
 
         services.AddIdentityServer(options =>
             {
@@ -33,17 +32,17 @@ public class Startup
             app.UseDeveloperExceptionPage();
         }
 
-        // uncomment if you want to add MVC
-        //app.UseStaticFiles();
-        //app.UseRouting();
+        
+        app.UseStaticFiles();
+        app.UseRouting();
         
         app.UseIdentityServer();
 
-        // uncomment, if you want to add MVC
-        //app.UseAuthorization();
-        //app.UseEndpoints(endpoints =>
-        //{
-        //    endpoints.MapDefaultControllerRoute();
-        //});
+        
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        { 
+            endpoints.MapDefaultControllerRoute();
+        });
     }
 }
